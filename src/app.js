ae2735f5e3f6e32303372342d7a86be5178d4536
Vinicius Lins <viniciusaclins@gmail.com>
2021-11-29 02:52:49 -0300

    Acrescentando arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const API_ROUTES = require('@routes/API.routes.js');
 
 app.use(express.json());
 app.use(cors())
+
+app.use('/', API_ROUTES)
 
 module.exports = app;
